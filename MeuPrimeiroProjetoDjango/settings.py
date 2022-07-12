9a95318fc1bc795b2ff5a0627bd697dbb89ee63a
Joao <joaovsmendes0127@gmail.com>
2022-07-11 21:31:10 -0300

    Primeira alteração

--- MeuPrimeiroProjetoDjango/settings.py
+++ MeuPrimeiroProjetoDjango/settings.py
@@ -76,12 +76,9 @@ WSGI_APPLICATION = 'MeuPrimeiroProjetoDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.dbe.backends.mysql',
         'NAME': 'BD_RH',
-        'USER': 'root',
-        # 'PASSWORD': 'DB_PASSWORD',
+        'USER': 'root', 
         'HOST': 'localhost',
         'PORT': '3306'
     }
